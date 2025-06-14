a9f6b72ebaedefc81a0bbca6ed4528803e382995
Francisco1825 <fdouglas1825@gmail.com>
2025-06-13 22:31:39 -0300

    Remover arquivo do Git LFS e controlar normalmente

--- Iac/iam.tf
+++ Iac/iam.tf
@@ -1,44 +1,69 @@
 resource "aws_iam_openid_connect_provider" "oidc-git" {
-    url = "https://token.actions.githubusercontent.com"
-    client_id_list = [
-        "sts.amazonaws.com"
-    ]
+  url = "https://token.actions.githubusercontent.com"
 
-    thumbprint_list = [
-        "d89e3bd43d5d909b47a18977aa9d5ce36cee184c"
-    ]
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
 
-    tags = {
-        IAC = true
-    }
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+
+  tags = {
+    IAC = true
+  }
 }
 
 resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
- 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Principal": {
-                "Federated": "arn:aws:iam::231224359494:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": [
-                        "sts.amazonaws.com"
-                    ],
-                    "token.actions.githubusercontent.com:sub": [
-                        "repo:Francisco1825/rocketseat.ci.api:ref:refs/heads/main"
-                    ]
-                }
-            }
+      {
+        "Effect": "Allow",
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Principal": {
+          "Federated": aws_iam_openid_connect_provider.oidc-git.arn
+        },
+        "Condition": {
+          "StringEquals": {
+            "token.actions.githubusercontent.com:aud": [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub": [
+              "repo:Francisco1825/rocketseat.ci.api:ref:refs/heads/main"
+            ]
+          }
         }
+      }
     ]
-})
+  })
+
+  inline_policy {
+    name = "ecr-app-permission"
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Sid      = "Statement1"
+          Effect   = "Allow"
+          Action   = [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken"
+          ]
+          Resource = "*"
+        }
+      ]
+    })
+  }
 
   tags = {
     IAC = true
--- Iac/main.tf
+++ Iac/main.tf
@@ -1,13 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.49.0"
+  terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "5.49.0"
+      }
     }
   }
-}
 
-provider "aws" {
-  alias = "global"
-  region = "us-east-1"
-}
+  provider "aws" {
+    region = "us-east-1"
+  }
